781a9f19945d502beb9bac938df3f35c909d8e76
Akhadjon Abdukhalilov <u1710021@akhadjons-macbook-pro.local>
2019-04-02 02:17:31 +0500

    Initial Commit

---
+++ KivaLoan/Loan.swift
@@ -0,0 +1,16 @@
+//
+//  Loan.swift
+//  KivaLoan
+//
+//  Created by Akhadjon Abdukhalilov on 3/31/19.
+//  Copyright © 2019 AppCoda. All rights reserved.
+//
+
+import Foundation
+
+struct Loan{
+    var name :String = ""
+    var country :String = ""
+    var use : String = ""
+    var amount : Int = 0
+}
